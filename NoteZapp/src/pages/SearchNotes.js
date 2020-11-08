2f584522ff56531becdbf41b17f0a8830f3745d0
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-11-08 20:15:45 +0530

    Incomplete

--- NoteZapp/src/pages/SearchNotes.js
+++ NoteZapp/src/pages/SearchNotes.js
@@ -2,44 +2,6 @@ import React from 'react'
 import tw from "twin.macro";
 import styled from "styled-components";
 import "./SearchNotes.css"
-import { Card, Button, CardTitle, CardText } from 'reactstrap';
-
-const Example = () => {
-  return (
-    <div>
-      <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button>Button</Button>
-      </Card>
-      <Card body inverse color="primary">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-      <Card body inverse color="success">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-      <Card body inverse color="info">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-      <Card body inverse color="warning">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-      <Card body inverse color="danger">
-        <CardTitle tag="h5">Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-    </div>
-  );
-};
 
 const Heading = styled.h1`
   ${tw`text-3xl text-center sm:text-4xl lg:text-5xl xl:text-6xl font-black text-gray-100 leading-snug -mt-24 sm:mt-0`}
@@ -51,7 +13,9 @@ function SearchNotes() {
     return (
         <div className= "heading">
            <Heading>See What's being Shared in your University!</Heading> 
-           <Example />
+           <h1> In this section the users will get the option to search for notes uploaded by others!
+               Couldn't finish it due to time constraints!
+           </h1>
         </div>
     )
 }
